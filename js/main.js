32cf3fb72366d00ee72db85214c21306df3b03c4
mac book pro <macbookpro@macs-MacBook-Pro.local>
2023-12-21 15:22:34 +0400

    restart

---
+++ js/main.js
@@ -0,0 +1,15 @@
+var daxilol = document.querySelector('.daxilol');
+
+function increaseSize() {
+    daxilol.style.fontSize = '18px';
+    daxilol.style.transition = 'font-size 0.6s';
+}
+
+function decreaseSize() {
+    daxilol.style.fontSize = 'inherit';
+    daxilol.style.transition = 'font-size 0.6s';
+}
+
+// forgot password
+
+
