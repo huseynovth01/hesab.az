fd3fb3e36a129ca709d15c49fd276260246d6014
mac book pro <macbookpro@macs-MacBook-Pro.local>
2023-12-21 18:46:37 +0400

    eye on or off

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
+var eye = document.querySelector(".eye-icon");
+var eyeoff = document.querySelector(".eye")
+var password = document.getElementById("password");
 
 
 
+// ---------call names----------
+
+
+
+// -----------textpassword------
+
+eye.addEventListener('click' , function(){
+eye.style.display = "none" ;   
+eyeoff.style.display= "block";
+password.type="password";
+})
+eyeoff.addEventListener('click' , function(){
+    eye.style.display = "block" ;   
+    eyeoff.style.display= "none";
+    password.type="text";
+    });
+
+// -----------textpassword------
