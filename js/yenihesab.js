b51bcf178ad55853b6e02563f808dcc79a2126bc
mac book pro <macbookpro@macs-MacBook-Pro.local>
2023-12-22 03:18:19 +0400

    yeni hesab restart

--- js/yenihesab.js
+++ js/yenihesab.js
@@ -1,34 +1,48 @@
 var form_input = document.querySelector(".form_input");
 var formcontrol = document.querySelector(".form-control");
 var mobilmessage = document.querySelector(".mobilmessage");
-var emailmessage = document.querySelector(".emailmessage");
 
-form_input.addEventListener("submit", function(e) {
+
+document.querySelector(".form_input").addEventListener("submit",function(e){
     e.preventDefault();
 
     if (formcontrol.value.trim() === "") {
-        mobilmessage.textContent = "Telefon nomrenizi daxil edin";
+        mobilmessage.textContent = "Mobil nomrenizi daxil edin";
         mobilmessage.style.color = "red";
         formcontrol.style.border = "1px solid red";
-    } else {
+    } 
+    else {
         mobilmessage.textContent = "";
         formcontrol.style.border = "1px solid black";
 
-        if (!formcontrol.value.match) {
-            mobilmessage.textContent = "Meselen +994 00 123 45 67";
+        if (!formcontrol.value.includes("+994 51 467 73 33")) {
+            mobilmessage.textContent = "Məsələn +994 51 467 73 33";
             mobilmessage.style.color = "red";
-            formcontrol.style.border = "1px solid black";
+            formcontrol.style.border = "1px solid red";
+        }
+    }
+   
+
+    // email
+
+
+    var emailmessage = document.querySelector(".emailmessage");
+    var formcontrolmail = document.querySelector(".form-controlmail")
+
+    if (formcontrolmail.value.trim() === "") {
+        emailmessage.textContent = "Emailinizi daxil edin";
+        emailmessage.style.color = "red";
+        formcontrolmail.style.border = "1px solid red";
+    } 
+    else {
+        emailmessage.textContent = "";
+        formcontrolmail.style.border = "1px solid black";
+
+        if (!formcontrolmail.value.includes("@")) {
+            emailmessage.textContent = "Məsələn hesab@23.az";
+            emailmessage.style.color = "red";
+            formcontrolmail.style.border = "1px solid red";
         }
     }
 
-    var firstname = document.getElementById("firstname").value;
-    var emailmessage = document.querySelector(".emailmessage");
-
-    if (firstname.trim() === "") {
-        emailmessage.textContent = "Emailinizi daxil edin";
-        emailmessage.style.color = "red";
-    } else {
-        emailmessage.textContent = "";
-        emailmessage.style.border = "1px solid black";
-    }
 });
