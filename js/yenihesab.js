cc73a1b393b964a0856d34e044a83cad71e43161
mac book pro <macbookpro@macs-MacBook-Pro.local>
2023-12-22 01:56:28 +0400

    restart

--- js/yenihesab.js
+++ js/yenihesab.js
@@ -7,28 +7,28 @@ form_input.addEventListener("submit", function(e) {
     e.preventDefault();
 
     if (formcontrol.value.trim() === "") {
-        mobilmessage.textContent = "Telefon numarasını girin";
+        mobilmessage.textContent = "Telefon nomrenizi daxil edin";
         mobilmessage.style.color = "red";
         formcontrol.style.border = "1px solid red";
     } else {
         mobilmessage.textContent = "";
         formcontrol.style.border = "1px solid black";
 
-        if (!formcontrol.value.match(/^\+994 \d{2} \d{3} \d{2} \d{2}$/)) {
+        if (!formcontrol.value.match) {
             mobilmessage.textContent = "Meselen +994 00 123 45 67";
             mobilmessage.style.color = "red";
-            formcontrol.style.border = "1px solid red";
+            formcontrol.style.border = "1px solid black";
         }
     }
 
     var firstname = document.getElementById("firstname").value;
-    var passwordmessage = document.querySelector(".passwordmessage");
+    var emailmessage = document.querySelector(".emailmessage");
 
     if (firstname.trim() === "") {
-        passwordmessage.textContent = "İsminizi girin";
-        passwordmessage.style.color = "red";
+        emailmessage.textContent = "Emailinizi daxil edin";
+        emailmessage.style.color = "red";
     } else {
-        passwordmessage.textContent = "";
-        passwordmessage.style.border = "1px solid black";
+        emailmessage.textContent = "";
+        emailmessage.style.border = "1px solid black";
     }
 });
